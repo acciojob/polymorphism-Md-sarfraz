f6b25654fc20017a38581a61f8aa3787586e3bb5
Md-sarfraz <mdsarfraz62070@gmail.com>
2024-12-21 23:53:13 +0530

    firs commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
+class Product{
+    public int product(int x, int y) {
+        return x*y;
+    }
+    public int product(int x, int y, int z) {
+        return x*y*z;
+    }
+    public double product(double x, double y) {
+        return x*y;
+    }
+}
 public class Main {
+    public static void main(String[] args) {
+        Product p=new Product();
+        int result1=p.product(2,3);
+        int result2=p.product(2,3,4);
+        double result3=p.product(2.5,3.5);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result2);
 
+
+    }
 }
