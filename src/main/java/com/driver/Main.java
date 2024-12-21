a760458389f933fd1638368af2d095f6674d8ea8
Md-sarfraz <mdsarfraz62070@gmail.com>
2024-12-22 00:01:08 +0530

    initil commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,17 +1,18 @@
 package com.driver;
 
-class Product{
-    public int product(int x, int y) {
-        return x*y;
-    }
-    public int product(int x, int y, int z) {
-        return x*y*z;
-    }
-    public double product(double x, double y) {
-        return x*y;
-    }
-}
+
 public class Main {
+    public static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
     public static void main(String[] args) {
         Product p=new Product();
         int result1=p.product(2,3);
